d2ab539f4ecd0622ea7366cee7db0f429e5a6242
Jonathan McCrary <jonathan.mccrary@me.com>
2023-10-30 12:27:12 -0500

    Update comments

--- PaylocityBenefitsCalculator/Api/Services/EmployeeService.cs
+++ PaylocityBenefitsCalculator/Api/Services/EmployeeService.cs
@@ -18,6 +18,14 @@ namespace Api.Services
             _mapper = mapper;
         }
 
+        /// <summary>
+        /// If it is found that an employee has more than one partner (spouse/domestic partner),
+        /// -1 is returned. This indicates to the controller that the business rule was violated.
+        /// This is a simple way to signify this--a more extensible solution would be to create a
+        /// new return type indicating the result of the insert.
+        /// </summary>
+        /// <param name="employee"></param>
+        /// <returns></returns>
         public int? AddEmployee(EmployeeDto employee)
         {
             if (employee.Dependents.Count > 0)
@@ -33,6 +41,14 @@ namespace Api.Services
             return _benefitsRepository.AddEmployee(_mapper.Map<EmployeeDto, Employee>(employee));
         }
 
+        /// <summary>
+        /// If it is found that an employee has more than one partner (spouse/domestic partner),
+        /// -1 is returned. This indicates to the controller that the business rule was violated.
+        /// This is a simple way to signify this--a more extensible solution would be to create a
+        /// new return type indicating the result of the insert.
+        /// </summary>
+        /// <param name="employee"></param>
+        /// <returns></returns>
         public async Task<int?> AddEmployeeAsync(EmployeeDto employee)
         {
             if (employee.Dependents.Count > 0)
@@ -62,14 +78,12 @@ namespace Api.Services
         public List<EmployeeDto> GetEmployees()
         {
             var employees = _benefitsRepository.GetEmployees();
-
             return _mapper.Map<List<Employee>, List<EmployeeDto>>(employees);
         }
 
         public async Task<List<EmployeeDto>> GetEmployeesAsync()
         {
             var employees = await _benefitsRepository.GetEmployeesAsync();
-
             return _mapper.Map<List<Employee>, List<EmployeeDto>>(employees);
         }
     }
